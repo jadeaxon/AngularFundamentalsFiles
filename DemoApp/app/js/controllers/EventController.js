c43ada5b420ab185ca7094f5fcd4bbfa244f01aa
Jeff Anderson <jeff.anderson@digecor.com>
2018-07-24 20:16:11 -0600

    Add controller EventController

---
+++ DemoApp/app/js/controllers/EventController.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// eventsApp was created in app.js.
+console.log("Registering EventController.");
+eventsApp.controller('EventController', function ($scope) {
+  // This is a social event.  Some kind of AngularJS-related gathering.
+  $scope.event = {
+    name: "AngularJS Bootcamp",
+    date: "2018-07-24",
+    time: "10:00 AM"
+  };
+
+});
+
+
